5dbbb9d145639081b2fffcf150512e7082464aa9
FayxChance <fayxchancetom@outlook.com>
2021-02-01 13:53:34 +0100

    First commit

---
+++ Utils.cpp
@@ -0,0 +1,10 @@
+//
+// Created by neptune on 01/02/2021.
+//
+
+#include "Utils.h"
+
+Vector3 prodVec(const Vector3 &v1, const Vector3 &v2) {
+    Vector3 v(v1.);
+    return ;
+}
---
+++ Utils.h
@@ -0,0 +1,14 @@
+//
+// Created by neptune on 01/02/2021.
+//
+#pragma once
+#ifndef TP2_UTILS_H
+#define TP2_UTILS_H
+
+#include <cmath>
+
+using namespace rt;
+
+Vector3 prodVec(const Vector3& v1, const  Vector3& v2);
+
+#endif //TP2_UTILS_H
